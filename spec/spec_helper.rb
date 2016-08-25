73132a64b0fa09c8c49bd3d874c978c810a18b36
Clifton Choiniere <crcnum4@gmail.com>
2016-08-24 22:03:38 -0400

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ require 'rspec'
 require 'rack/test'
 require 'webmock/rspec'
 require 'omniauth'
-require 'omniauth-twitter'
+require 'omniauth-twitter_band'
 
 RSpec.configure do |config|
   config.include WebMock::API
